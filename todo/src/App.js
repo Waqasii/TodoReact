0b3c41da9cc15ef4f00b807ccac4a2fe1197ed14
waqasii <waqas199888@gmail.com>
2023-03-06 13:29:26 +0100

    add todo item

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,12 +1,16 @@
-
 import Header from "./components/Header.js";
 import Form from "./components/Form.js";
+import { useState } from "react";
 
 function App() {
+  const [todo, settodo] = useState("")
+  const [todoList, setTodoList] = useState([])
   return (
     <div className="App">
       <Header />
-      <Form />
+      <Form
+        todo={todo} setTodo={settodo} todoList={todoList} setTodoList={setTodoList}
+      />
     </div>
   );
 }
--- todo/src/components/Form.js
+++ todo/src/components/Form.js
@@ -1,13 +1,25 @@
 import styles from "../style.module.css"
 
-const Form = () => {
+const Form = ({ todo, setTodo, todoList, setTodoList }) => {
+
+    const handleChange = (event) => {
+        setTodo(event.target.value)
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        setTodoList([...todoList, todo])
+        setTodo("")
+    }
     return (
         <div className={styles.formdiv}>
-            <form className={styles.todoform}>
-                <input className={styles.todoinput} placeholder="Add todo item"></input>
+            <form className={styles.todoform} onSubmit={handleSubmit}>
+                <input className={styles.todoinput} value={todo} onChange={handleChange} placeholder="Add todo item"></input>
                 <button className={styles.addbutton} type="submit">+</button>
+
             </form>
         </div>
+
+
     )
 }
 
